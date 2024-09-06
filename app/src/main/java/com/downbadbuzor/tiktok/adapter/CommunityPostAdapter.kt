214b8b73c1fba82f34cfc53b9fdb663f9fa44eb3
Chibuzor <chibuzorm26@gmail.com>
2024-09-06 16:59:16 +0100

    post buttons init

--- app/src/main/java/com/downbadbuzor/tiktok/adapter/CommunityPostAdapter.kt
+++ app/src/main/java/com/downbadbuzor/tiktok/adapter/CommunityPostAdapter.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -27,7 +28,7 @@ import java.util.Date
 import java.util.Locale
 
 class CommunityPostAdapter(private val activity: Activity):
-    RecyclerView.Adapter<CommunityPostAdapter.PostViewHolder>(){
+    RecyclerView.Adapter<CommunityPostAdapter.PostViewHolder>() {
 
     private val posts = mutableListOf<CommuinityModel>()
 
@@ -35,6 +36,7 @@ class CommunityPostAdapter(private val activity: Activity):
         posts.addAll(newVideos)
         notifyDataSetChanged()
     }
+
     fun clearPosts() {
         posts.clear()
         notifyDataSetChanged()
@@ -49,84 +51,116 @@ class CommunityPostAdapter(private val activity: Activity):
     inner class PostViewHolder(private val binding: PostItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        private var isLiked = false
+        private var isStarred = false
 
-            fun bindPost(postModel: CommuinityModel) {
+        fun bindPost(postModel: CommuinityModel) {
 
-                val postDictionary = hashMapOf(
-                    "postId" to postModel.postId,
-                    "picture" to postModel.picture,
-                    "content" to postModel.content,
-                    "uploaderId" to postModel.uploaderId,
-                    "createdTime" to  formatDate(postModel.createdTime.toDate())
-                )
-                val bundle = Bundle()
-                for ((key, value) in postDictionary) {
-                    bundle.putString(key, value.toString()) // Assuming all values can be converted to strings
-                }
+            val postDictionary = hashMapOf(
+                "postId" to postModel.postId,
+                "picture" to postModel.picture,
+                "content" to postModel.content,
+                "uploaderId" to postModel.uploaderId,
+                "createdTime" to formatDate(postModel.createdTime.toDate())
+            )
+            val bundle = Bundle()
+            for ((key, value) in postDictionary) {
+                bundle.putString(
+                    key,
+                    value.toString()
+                ) // Assuming all values can be converted to strings
+            }
 
 
+            //bind the user data
+            Firebase.firestore.collection("users")
+                .document(postModel.uploaderId)
+                .get()
+                .addOnSuccessListener {
+                    val userModel = it?.toObject(UserModel::class.java)
+                    userModel?.apply {
+                        binding.username.text = username
+                        //bind profile
+                        Glide.with(binding.profileIcon).load(profilePic)
+                            .circleCrop()
+                            .apply(
+                                RequestOptions().placeholder(R.drawable.icon_account_circle)
+                            )
+                            .into(binding.profileIcon)
 
-                //bind the user data
-                Firebase.firestore.collection("users")
-                    .document(postModel.uploaderId)
-                    .get()
-                    .addOnSuccessListener {
-                        val userModel = it?.toObject(UserModel::class.java)
-                        userModel?.apply {
-                            binding.username.text = username
-                            //bind profile
-                            Glide.with(binding.profileIcon).load(profilePic)
-                                .circleCrop()
-                                .apply(
-                                    RequestOptions().placeholder(R.drawable.icon_account_circle)
-                                )
-                                .into(binding.profileIcon)
-
-                            binding.profileIcon.setOnClickListener {
-                                val intent = Intent(
-                                    activity,
-                                    ProfileActivity::class.java
-                                )
-                                intent.putExtra("profile_user_id", id)
-                                activity.startActivity(intent)
-                            }
+                        binding.profileIcon.setOnClickListener {
+                            val intent = Intent(
+                                activity,
+                                ProfileActivity::class.java
+                            )
+                            intent.putExtra("profile_user_id", id)
+                            activity.startActivity(intent)
                         }
-
                     }
 
-                Glide.with(binding.postImage)
-                    .load(postModel.picture)
-                    .override(1000, 600)
-                    .into(binding.postImage)
-                if (postModel.content != "") {
-                    binding.postContent.visibility = View.VISIBLE
-                }
-                if (postModel.picture != "") {
-                    binding.postImage.visibility = View.VISIBLE
-                }
-                binding.timestampText.text = "• ${formatDate(postModel.createdTime.toDate())}"
-                binding.postContent.text = postModel.content
-
-                binding.postImage.setOnClickListener {
-                    val intent = Intent(
-                        activity,
-                        FullScreenImage::class.java
-                    )
-                    intent.putExtra("image_url", postModel.picture)
-                    activity.startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(activity).toBundle())
-                }
-                binding.postBody.setOnClickListener {
-                    val intent = Intent(
-                        activity,
-                        FullPost ::class.java
-                    )
-                    intent.putExtras(bundle)
-                    activity.startActivity(intent)
                 }
 
-
-
+            Glide.with(binding.postImage)
+                .load(postModel.picture)
+                .override(1000, 600)
+                .into(binding.postImage)
+            if (postModel.content != "") {
+                binding.postContent.visibility = View.VISIBLE
             }
+            if (postModel.picture != "") {
+                binding.postImage.visibility = View.VISIBLE
+            }
+            binding.timestampText.text = "• ${formatDate(postModel.createdTime.toDate())}"
+            binding.postContent.text = postModel.content
+
+            val zoomInAnim = AnimationUtils.loadAnimation(activity, R.anim.zoom_in)
+
+            binding.postImage.setOnClickListener {
+                val intent = Intent(
+                    activity,
+                    FullScreenImage::class.java
+                )
+                intent.putExtra("image_url", postModel.picture)
+                activity.startActivity(
+                    intent,
+                    ActivityOptions.makeSceneTransitionAnimation(activity).toBundle()
+                )
+            }
+
+            binding.heart.setOnClickListener {
+                if (isLiked) {
+                    binding.heart.setImageResource(R.drawable.like_outline)
+
+                } else {
+                    binding.heart.setImageResource(R.drawable.like_filled_red)
+
+                }
+                binding.heart.startAnimation(zoomInAnim)
+                isLiked = !isLiked
+            }
+            binding.star.setOnClickListener {
+                if (isStarred) {
+                    binding.star.setImageResource(R.drawable.star_outline)
+
+                } else {
+                    binding.star.setImageResource(R.drawable.gold_star)
+
+                }
+                binding.star.startAnimation(zoomInAnim)
+                isStarred = !isStarred
+            }
+
+            binding.postBody.setOnClickListener {
+                val intent = Intent(
+                    activity,
+                    FullPost::class.java
+                )
+                intent.putExtras(bundle)
+                activity.startActivity(intent)
+            }
+
+
+        }
 
 
     }
@@ -139,7 +173,8 @@ class CommunityPostAdapter(private val activity: Activity):
     override fun getItemCount(): Int = posts.size
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-         holder.bindPost(posts[position])
+        holder.bindPost(posts[position])
     }
-
 }
+
+
